4af18d9e215c51c02aa56a367339e855070580fc
Thauanny <thauanny.ramos.124@ufrn.edu.br>
2023-08-18 11:09:38 -0300

    configuração ambiente https

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,13 @@ import 'package:flutter_application_1/src/shared/core/env/env.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+
   runApp(
     AppModule(
       env: Env.get("ENV"),
       envLaunch: Env.get("ENV_LAUNCH"),
+      baseUrl: Env.get("BASE_URL"),
+      prefixo: Env.get("PREFIXO"),
     ),
   );
 }
